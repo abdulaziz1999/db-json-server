1cc31693de346a20dc603ed770fd0ca6df1e30a5
Abdul Aziz <azizmentor96@gmail.com>
2024-01-26 13:51:12 +0700

    update api server

--- api/server.js
+++ api/server.js
@@ -1,29 +1,31 @@
-const jsonServer = require('json-server');
-const fs = require('fs');
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server')
 
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const server = jsonServer.create()
 
-server.use(middlewares);
+// Uncomment to allow write operations
+const fs = require('fs')
+const path = require('path')
+const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, "utf-8");
+const db = JSON.parse(data);
+const router = jsonServer.router(db)
+
+// Comment out to allow write operations
+// const router = jsonServer.router('db.json')
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
-}));
-server.use(router);
-
+}))
+server.use(router)
 server.listen(3000, () => {
-    console.log('JSON Server is running');
-});
-
-// Tambahkan kode untuk membaca db.json
-fs.readFile('db.json', 'utf8', function (err, data) {
-    if (err) {
-        console.error('Error reading db.json:', err);
-        return;
-    }
-    console.log('Contents of db.json:', data);
-});
+    console.log('JSON Server is running')
+})
 
 // Export the Server API
-module.exports = server;
+module.exports = server
